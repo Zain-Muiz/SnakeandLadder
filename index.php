af40b45816e4a086cec7c54d78a5bc3d3b3d93a4
Zain-Muiz <60746360+Zain-Muiz@users.noreply.github.com>
2021-03-22 14:38:24 +0530

    message: initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>snakesladders</title>
+
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/p5@1.3.0/lib/p5.min.js"></script>
+	
+	<script type="text/javascript" src="player.js"></script>
+	<script type="text/javascript" src="tile.js"></script>
+	<script type="text/javascript" src="sketch.js"></script>
+</head>
+<body>
+<img src="Images/dice1.jpg"><button onclick="player.rollDie()"></button></img>
+</body>
+</html>
